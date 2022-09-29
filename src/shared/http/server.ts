417a4049610b913f1a5d2ffabdaeb025d7c9e80e
deividadrian <deividadrian24@gmail.com>
2022-09-29 00:12:25 -0300

    Customizando o tratamento de Erros da aplicação

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import router from './roots';
+import AppError from '@shared/errors/AppErros';
 
 const app = express();
 
@@ -9,5 +10,18 @@ app.use(cors());
 app.use(express.json());
 
 app.use(router);
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if(error instanceof AppError) {
+    return response.status(error.statuscode).json({
+      status: 'error',
+      message: error.message,
+    })
+  }
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+})
 
 app.listen(3000, () => {console.log('Server running port 3000!')});
